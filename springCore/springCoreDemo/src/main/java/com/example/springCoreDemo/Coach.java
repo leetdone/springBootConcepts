ab394a008983ba06fae455476393d2db3e948ad3
leetdone <lindong_ye@163.com>
2023-04-22 14:11:00 -0400

    init

---
+++ springCore/springCoreDemo/src/main/java/com/example/springCoreDemo/Coach.java
@@ -0,0 +1,6 @@
+package com.example.springCoreDemo;
+
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ springCore/springCoreDemo/src/main/java/com/example/springCoreDemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.example.springCoreDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice bowling for 15 minutes!";
+    }
+}
